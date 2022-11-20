a64164e8fba34041418e9d46dedb00f3a5e1bd99
Treasure Onoja <treasureonoja@Treasures-MacBook-Air.local>
2022-11-20 21:11:16 +0000

    addd start script

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
 
 const app = express();
+
+app.use(morgan('combined'));
 
 app.get('/', (req, res)=>{
     res.send('Hello from my app');
 })
 
 app.listen(3000, ()=>{
-    console.log('Listening on port 3000');
+    debug(`Listening on port ${chalk.green('3000')}`);
 })
